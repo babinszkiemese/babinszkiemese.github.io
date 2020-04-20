a5f4bf053685519005f7ab4e76f034c34348689c
Emese Babinszki <63725802+babinszkiemese@users.noreply.github.com>
2020-04-20 17:21:11 +0200

    remove the consolelogs

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-console.log(`EmeseBabinszki`)
 
-let myVariable = false;
-if (myVariable !== true) {
-  console.log('The condition has been evaluated to true');
-}
-let a = 5
-if(a<5) {
-    console.log(a/2)
-} else{
-    console.log(a*2)
-}
-a=1
-if(a<5) {
-    console.log(a/2)
-} else{
-    console.log(a*2)
-}
-let bookcount = 26
-for(let i=0; i< bookcount; i++) {
- console.log('moving my little lovelybooks')
-}
-bookcount=100
-for(let i=0; i< bookcount; i++) {
-    console.log('moving my little lovelybooks')
-   }
-for(let i=1;i<10;i= i+2){
-    console.log(i)
-}
-for(let i=1;i<10;i){
-    console.log(i)
-}
 
 
 
